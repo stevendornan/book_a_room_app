922851de4a8c0f3a2e7d40b8330c0dd56970222f
Steven Dornan <steven.dornan1993@gmail.com>
2025-01-10 05:52:42 +0000

    Implemented Room Card

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,15 @@
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import rooms from "@/data/rooms.json";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms available at th moment</p>
+      )}
+    </>
+  );
 }
